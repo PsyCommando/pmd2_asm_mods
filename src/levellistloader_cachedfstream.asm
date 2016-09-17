a4874b71550c432a799df0873f09387cda5bc4c8
psy_commando <psycommando@gmail.com>
2016-09-16 22:31:26 -0400

    Works correctly with file streams
    * Works with file streams, but for some reasons, if I fill the rest of
    the old table with 0s instead of 0xFF, the game bugs out.. Something
    else must be accessing the table through some other mean?

--- src/levellistloader_cachedfstream.asm
+++ src/levellistloader_cachedfstream.asm
@@ -1,6 +1,6 @@
 ; For use with ARMIPS v0.7d
 ; By: psycommando@gmail.com
-; 2016/08/16
+; 2016/09/16
 ; For Explorers of Sky North American ONLY!
 ; ------------------------------------------------------------------------------
 ; Copyright © 2016 Guillaume Lavoie-Drapeau <psycommando@gmail.com>
@@ -8,23 +8,15 @@
 ; terms of the Do What The Fuck You Want To Public License, Version 2,
 ; as published by Sam Hocevar. See http://www.wtfpl.net/ for more details.
 ; ------------------------------------------------------------------------------
-.relativeinclude on
+;.relativeinclude on
 .nds
 .arm
-.include "cmn_eos.asm"
 
-.definelabel PPMD_GameVer,    0  ;Symbol for telling the library we're compiled for EoS North America "0"
+;Since we don't need to load it with streams, don't do anything!
+    TryLoadLevelList:
+      bx r14
+    ;END
 
-;.definelabel LoadFileFromRom, 0x2008C3C
-;.definelabel HandleSIR0,      0x201F4B4
-;.definelabel DebugPrint,      0x200C240
-
-;First do am9.bin
-.open "../bin_src/arm9.bin", "../bin_out/arm9.bin", 0x02000000
-    ;Implement our own file loading function to load the list as a sir0!
-    .org 0x020A46EC ;write over the actual table
-    ;.area 0x20A6910 - .;0x2224 ;We got at most 8,740 bytes to write stuff here, so there's plenty of room!
-    .area 0x20A68BC - .
 ;Check if we need to load the level list for this entry! (r0 = level id, r1=nothing) Return in r0, 1 if should load, 0 if not
     CheckIfShouldGetLvlEntryFromFile:
       ldr r1,=LevelEntryBuffer_lastlvlid
@@ -40,7 +32,7 @@
       .align 4
     ;END
 
-;LoadLvlEntryFromFileOrCache (r0 = levelid) return in r0 the address of the entry in memory
+;LevelListAccessor (r0 = levelid) return in r0 the address of the entry in memory
     LevelListAccessor:
       push r1,r4,r14
       mov r4,r0
@@ -157,114 +149,14 @@
         .pool
         LevelEntryBuffer_lastlvlid:
           dcd 0 ;holds the last levelid so we can cache calls to this and not reload the file all the time
-          ;.align 4
         LevelEntryBuffer_lastentry:
           dcd 0,0,0 ;an entry is 12 bytes
         .align 4
         LevelEntryBuffer_lastname:
           dcb 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 ;reserve 16bytes string for writing the last string for the last entry here!
-          ;.align 4
         LevelList_PointerBuffer:
           dcd 0
-          ;.align 4
         LevelList_FileStream: ;Put the file stream struct here!
           defs 0x48,0
         .align 4
       ;END
-
-;********************************
-; A few customized functions for making a more seamless hooking!
-;********************************
-;Same as above, except it returns the pointer to the string of the level, instead of the entry!
-        LevelListStringAccessor:
-          push r14
-          bl LevelListAccessor  ;Get the pointer to the entry
-          ;add r0,r0,8h        ;Increment to get the pointer to the string
-          ldr r0,[r0,8h]          ;Read the pointer to the string into r0
-          pop r15
-          .pool
-          .align 4
-        ;END LevelListStringAccessor
-
-;Helper for getting the first hword and return it into r0.
-        LevelListFirstHWORDGet:
-            push  r14
-            bl    LevelListAccessor
-            ldrsh r0,[r0]
-            pop   r15
-            .pool
-            .align 4
-        ;END
-
-
-
-        LevelListFPath:
-            .ascii "rom0:BALANCE/level_list.bin"      ;This is the name of SIR0 file that'll contain our level table!
-            dcb 0 ;Put ending 0
-        .align 4 ;align the string on 4bytes
-;Fill up the rest with junk so we know if something went wrong
-        .fill  (0x20A68BC - .), 255 ;Null out the rest of the table
-    .endarea
-
-;===============================================================================
-; Replace all instances of the table address with our hacked functions
-;===============================================================================
-
-;-------------------------------------
-; Level Getter2 Hook
-;-------------------------------------
-    .org 0x2065014
-    .area 0x2065050 - 0x2065014
-      push r1,r14
-      mvn     r1,0h
-      cmp     r0,r1
-      beq     @@Abort
-      ;mov     r1,0Ch
-      ;smulbb  r1,r0,r1
-      ;ldr     r0,=20A5488h
-      ;ldrsh   r0,[r0,r1]
-      bl LevelListFirstHWORDGet
-      cmp     r0,5h
-      cmpne   r0,6h
-      cmpne   r0,8h
-      moveq   r0,0h
-      ;bxeq    r14
-      popeq r1,r15
-      @@Abort:
-      mov     r0,1h
-      ;bx      r14
-      pop r1,r15
-      .pool
-    .endarea
-
-;-------------------------------------
-; Level String Getter Hook
-;-------------------------------------
-    .org 0x02064FFC
-    .area 0x18 ; we got 24 bytes max here
-        push r14
-        ;Call our modified routine
-        bl LevelListStringAccessor ;We want only the string!!
-        pop r15
-        .pool
-        ;.fill (0x02064FFC + 0x18) - .,0
-    .endarea
-    ;END
-
-;-------------------------------------
-; FontLoader Hook (For loading as SIR0 only)
-;-------------------------------------
-    ;.org 0x2025AD8
-    ;.area (0x2025B7C - 0x2025AD8)
-      ;push r14
-      ;bl ReplacedFontLoader
-      ;pop r15
-      ;.pool
-      ;.fill (0x2025B7C - .),0
-    ;.endarea
-    ;END
-
-
-.close ;Close arm9.bin
-
-.include "levellistloader_overlay11.asm"
